42594594f46a8972868aa26cd72bcbcf08e0eab2
Christabelle Bosson <christabellebosson@me.com>
2012-07-23 17:42:22 -0400

    Added an airline entity in the db.

---
+++ FlightViewer/FlightViewer/Airline.m
@@ -0,0 +1,18 @@
+//
+//  Airline.m
+//  FlightViewer
+//
+//  Created by Christabelle Bosson on 07/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Airline.h"
+#import "FlightInfo.h"
+
+
+@implementation Airline
+
+@dynamic name;
+@dynamic airline;
+
+@end
